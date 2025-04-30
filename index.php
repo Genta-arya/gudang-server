3805e9c3212b8b59be10fe9781ca4533f6d4f323
GentaArya <mgentaarya@yahoo.com>
2025-04-30 12:00:13 +0700

    update

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Contoh routing sederhana jika perlu
-if ($_SERVER['REQUEST_URI'] === '/api/gudang-farmasi/permintaan') {
-    require_once 'service/permintaan.php';  // Pastikan path ke service yang benar
-}
-?>
