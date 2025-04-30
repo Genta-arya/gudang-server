97bcf8efc2f16afb985b7afc5ba4099437fa0260
GentaArya <mgentaarya@yahoo.com>
2025-04-30 12:08:16 +0700

    update

--- service/report.php
+++ service/report.php
@@ -1,10 +1,15 @@
 <?php
 require_once '../model/db.php';
+header("Access-Control-Allow-Origin: *");  
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header('Content-Type: application/json');
 
+
+
 if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
-    http_response_code(200);
-    exit();
+    http_response_code(200);  
+    exit();  
 }
 
 
--- service/stokopname.php
+++ service/stokopname.php
@@ -1,5 +1,17 @@
 <?php
 header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");  
+header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Content-Type: application/json');
+
+
+
+if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS') {
+    http_response_code(200);  
+    exit();  
+}
+
 require_once "../model/db.php";  
 
 try {
